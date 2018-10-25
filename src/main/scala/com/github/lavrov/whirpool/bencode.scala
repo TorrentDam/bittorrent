3518079fa159473dac59a3f1290b01eb6f5856d6
Vitaly Lavrov <lavrovvv@gmail.com>
2018-10-25 16:16:33 +0200

    Bencode parser

---
+++ src/main/scala/com/github/lavrov/whirpool/bencode.scala
@@ -0,0 +1,40 @@
+package com.github.lavrov.whirpool
+
+import atto._, Atto._, syntax.refined._
+import eu.timepit.refined.numeric._
+import cats.syntax.functor._
+
+object bencode {
+
+  sealed trait Bencode
+  object Bencode {
+    case class String(value: java.lang.String) extends Bencode
+    case class Integer(value: scala.Long) extends Bencode
+    case class List(values: collection.immutable.List[Bencode]) extends Bencode
+    case class Dictionary(values: Map[java.lang.String, Bencode]) extends Bencode
+  }
+
+  val parser: Parser[Bencode] = {
+
+    val stringParser: Parser[Bencode.String] = int.refined[Positive] <~ char(':') flatMap {
+      number => manyN(number.value, anyChar).map(_.mkString).map(Bencode.String)
+    }
+
+    val integerParser: Parser[Bencode.Integer] = char('i') ~> long <~ char('e') map {
+      number => Bencode.Integer(number)
+    }
+
+    val listParser: Parser[Bencode.List] = char('l') ~> many(delay(parser)) <~ char('e') map {
+      elems => Bencode.List(elems)
+    }
+
+    val keyValueParser = stringParser ~ delay(parser) map { case (Bencode.String(key), value) => (key, value) }
+
+    val dictionaryParser: Parser[Bencode.Dictionary]  = char('d') ~> many(keyValueParser) <~ char('e') map {
+      elems => Bencode.Dictionary(elems.toMap)
+    }
+
+    stringParser.widen[Bencode] | integerParser.widen | listParser.widen | dictionaryParser.widen
+  }
+
+}
