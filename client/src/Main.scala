18d6c60dde61f3129dc9d1fb634026fe85dbc45b
Vitaly Lavrov <lavrovvv@gmail.com>
2019-07-10 02:03:00 +0200

    Download button

--- client/src/Main.scala
+++ client/src/Main.scala
@@ -1,7 +1,10 @@
 import slinky.web.ReactDOM
 import slinky.hot
+import slinky.web.html._
+import slinky.core.CustomAttribute
 
 import org.scalajs.dom
+import scala.scalajs.js
 
 import react_md._
 import app.App
@@ -11,10 +14,21 @@ object Main {
   def main(args: Array[String]): Unit = {
     val container = dom.document.getElementById("root")
     val app =
-      Toolbar(
-        title = "BitTorrent",
-        themed = true,
-        actions = MenuButton("main-menu", Seq("Settings", "Help", "Feedback"))("more_vert")
+      div(
+        Toolbar(
+          title = "BitTorrent",
+          themed = true,
+          actions = (MenuButton("main-menu", Seq("Settings", "Help", "Feedback"))("more_vert"): js.Object)
+        ),
+        Grid()(
+          Cell(size = 3)(
+            Toolbar(
+              actions = Button(flat = true, primary = true)("Download"): js.Object,
+            )(
+              TextField(id = "magnet-link-download", label = "Magnet link"),
+            )
+          )
+        )
       )
     ReactDOM.render(app, container)
   }
--- client/src/react-md.scala
+++ client/src/react-md.scala
@@ -6,20 +6,54 @@ import slinky.web.html._
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.{JSExportTopLevel, JSImport}
+import scala.scalajs.js.UndefOr
 
 @js.native
 @JSImport("react-md", JSImport.Default)
 private object imports extends js.Object {
   val Toolbar: js.Object = js.native
   val MenuButton: js.Object = js.native
+  val TextField: js.Object = js.native
+  val Paper: js.Object = js.native
+  val Grid: js.Object = js.native
+  val Cell: js.Object = js.native
+  val Button: js.Object = js.native
 }
 
 @react object Toolbar extends ExternalComponentWithAttributes[*.tag.type] {
-  case class Props(title: String, themed: Boolean, actions: js.Object)
+  case class Props(
+    title: UndefOr[String] = js.undefined, 
+    themed: Boolean = false,
+    actions: UndefOr[js.Object] = js.undefined
+  )
   val component = imports.Toolbar
 }
 
 @react object MenuButton extends ExternalComponentWithAttributes[*.tag.type] {
   case class Props(id: String, menuItems: Seq[String], icon: Option[Boolean] = Some(true))
   val component = imports.MenuButton
+}
+
+@react object TextField extends ExternalComponentWithAttributes[*.tag.type] {
+  case class Props(id: String, label: String, toolbar: Boolean = false)
+  val component = imports.TextField
+}
+
+@react object Paper extends ExternalComponentWithAttributes[*.tag.type] {
+  case class Props(zDepth: Int)
+  val component = imports.Paper
+}
+
+@react object Grid extends ExternalComponentWithAttributes[*.tag.type] {
+  type Props = Unit
+  val component = imports.Grid
+}
+
+@react object Cell extends ExternalComponentWithAttributes[*.tag.type] {
+  case class Props(size: Int)
+  val component = imports.Cell
+}
+@react object Button extends ExternalComponentWithAttributes[*.tag.type] {
+  case class Props(flat: Boolean, primary: Boolean, swapTheming: UndefOr[Boolean] = js.undefined)
+  val component = imports.Button
 }
