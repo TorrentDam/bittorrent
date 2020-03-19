9e26c5faf340b752949d5cd699d232a3210962ed
Vitaly Lavrov <lavrovvv@gmail.com>
2020-03-19 23:54:33 +0100

    Guarantee memory cleanup when piece is downloaded

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM openjdk:14-slim
 
 COPY ./out/server/assembly/dest/out.jar /opt/server.jar
 
-ENTRYPOINT ["java", "-XX:G1PeriodicGCSystemLoadThreshold=0.0", "-XX:G1PeriodicGCInterval=30000", "-Xmx700m", "-jar", "/opt/server.jar"]
+ENTRYPOINT ["java", "-XX:MinHeapFreeRatio=10", "-XX:MaxHeapFreeRatio=20", "-XX:G1PeriodicGCInterval=60000", "-Xmx700m", "-jar", "/opt/server.jar"]
--- server/src/Multiplexer.scala
+++ server/src/Multiplexer.scala
@@ -12,25 +12,31 @@ object Multiplexer {
 
   def apply[F[_]](request: Int => F[ByteVector])(implicit F: Concurrent[F]): F[Multiplexer[F]] = {
     for {
-      pieces <- Ref.of(Map.empty[Int, Deferred[F, ByteVector]])
+      pieces <- Ref.of(Map.empty[Int, Deferred[F, Either[Throwable, ByteVector]]])
     } yield new Multiplexer[F] {
-      def get(index: Int): F[ByteVector] =
+      def get(index: Int): F[ByteVector] = {
+        val cleanup =
+          pieces.update { pieces =>
+            pieces.removed(index)
+          }
         for {
           effect <- pieces.modify { pieces =>
             pieces.get(index) match {
               case Some(deferred) => (pieces, deferred.get)
               case _ =>
-                val deferred = Deferred.unsafe[F, ByteVector]
+                val deferred = Deferred.unsafe[F, Either[Throwable, ByteVector]]
                 val updated = pieces.updated(index, deferred)
-                val effect = request(index).flatMap(deferred.complete).start >> deferred.get
+                val effect =
+                  request(index).attempt
+                    .flatTap(deferred.complete)
+                    .guarantee(cleanup)
                 (updated, effect)
             }
           }
           result <- effect
-          _ <- pieces.update { pieces =>
-            pieces.removed(index)
-          }
+          result <- F.fromEither(result)
         } yield result
+      }
     }
   }
 }
