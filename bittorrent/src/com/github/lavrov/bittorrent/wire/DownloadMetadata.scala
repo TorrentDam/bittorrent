f0e5f70e606621d56310cca25776bb64ed0aa096
Vitaly Lavrov <lavrovvv@gmail.com>
2020-06-24 23:45:00 +0200

    Timeout on metadata download

--- bittorrent/src/com/github/lavrov/bittorrent/wire/DownloadMetadata.scala
+++ bittorrent/src/com/github/lavrov/bittorrent/wire/DownloadMetadata.scala
@@ -1,23 +1,33 @@
 package com.github.lavrov.bittorrent.wire
 
 import cats.implicits._
-import cats.effect.Sync
+import cats.effect.implicits._
+import cats.effect.{Concurrent, Sync, Timer}
 import com.github.lavrov.bittorrent.MetaInfo
 import fs2.Stream
 
+import scala.concurrent.duration._
+
 object DownloadMetadata {
 
-  def apply[F[_]](connections: Stream[F, Connection[F]])(implicit F: Sync[F]): F[MetaInfo] =
+  def apply[F[_]](connections: Stream[F, Connection[F]])(implicit F: Concurrent[F], timer: Timer[F]): F[MetaInfo] =
     connections
-      .evalMap(_.extensionApi)
-      .map(_.utMetadata)
-      .collect {
-        case Some(utMetadata) => utMetadata
-      }
-      .evalMap(_.fetch.attempt)
+      .evalMap(connection =>
+        DownloadMetadata(connection)
+          .timeout(10.seconds)
+          .attempt
+      )
       .collectFirst {
         case Right(metadata) => metadata
       }
       .compile
       .lastOrError
+
+  def apply[F[_]](connection: Connection[F])(implicit F: Sync[F]): F[MetaInfo] =
+    connection.extensionApi
+      .flatMap(_.utMetadata.liftTo[F](UtMetadataNotSupported()))
+      .flatMap(_.fetch)
+
+  case class UtMetadataNotSupported() extends Throwable("UtMetadata is not supported")
+
 }
