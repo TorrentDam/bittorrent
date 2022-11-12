aeb18271004e2a52c8ce3cda0fa1a32a2c72d46a
Vitaly Lavrov <lavrovvv@gmail.com>
2022-11-12 16:13:25 +0100

    Revert "Delete FileMapping for now"
    This reverts commit 0d0feec462f8fe874bcea1c133d73c4879b8c4ce.

---
+++ bittorrent/src/test/scala/com/github/lavrov/bittorrent/FileMappingSpec.scala
@@ -0,0 +1,51 @@
+package com.github.lavrov.bittorrent
+
+import scodec.bits.ByteVector
+import TorrentMetadata.File
+
+class FileMappingSpec extends munit.FunSuite {
+
+  test("all files in one piece") {
+    val metadata = TorrentMetadata(
+      name = "test",
+      pieceLength = 10,
+      pieces = ByteVector.empty,
+      files = List(
+        File(5, Nil),
+        File(3, Nil),
+        File(2, Nil)
+      )
+    )
+    val result = FileMapping.fromMetadata(metadata)
+    val expectation = FileMapping(
+      List(
+        Span(0, 0, 0, 5, 10),
+        Span(0, 5, 0, 8, 10),
+        Span(0, 8, 1, 0, 10)
+      )
+    )
+    assert(result == expectation)
+  }
+
+  test("file spans multiple pieces") {
+    val metadata = TorrentMetadata(
+      name = "test",
+      pieceLength = 10,
+      pieces = ByteVector.empty,
+      files = List(
+        File(5, Nil),
+        File(13, Nil),
+        File(2, Nil)
+      )
+    )
+    val result = FileMapping.fromMetadata(metadata)
+    val expectation = FileMapping(
+      List(
+        Span(0, 0, 0, 5, 10),
+        Span(0, 5, 1, 8, 10),
+        Span(1, 8, 2, 0, 10)
+      )
+    )
+    assert(result == expectation)
+  }
+}
